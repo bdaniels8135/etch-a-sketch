190732fe1f427df4c2fac9aa25ed87ce145d723e
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-06-23 09:26:52 -0400

    Create buildGrid function and run on page load

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const ui = Object.freeze({
+    gridContainer: document.querySelector('#grid-container')
+})
+
 function buildGrid(dimension = 16) {
-    ...
+    let gridRow;
+    let gridElement;
+    for (let i = 0; i < dimension; i++) {
+        gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        ui.gridContainer.appendChild(gridRow);
+        for (let j = 0; j < dimension; j++) {
+            gridElement = document.createElement('div');
+            gridElement.classList.add('grid-element');
+            gridRow.appendChild(gridElement);
+        }
+    }
 }
-
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     buildGrid();

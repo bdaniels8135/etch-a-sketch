fd8d7334d9dc40c7f23e4aa70d64dab4cd669f7d
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-06-24 00:23:24 -0400

    Add button click listeners and resolve functions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const ui = Object.freeze({
     gridContainer: document.querySelector('#grid-container'),
     sizeButtons: document.querySelectorAll('.button.size'),
     colorButtons: document.querySelectorAll('.button.color'),
-    gridButtons: document.querySelectorAll('.button.grid-background')
+    gridBackgroundButtons: document.querySelectorAll('.button.grid-background')
 })
 
 function buildGrid(dimension = 16) {
@@ -18,6 +18,48 @@ function buildGrid(dimension = 16) {
             gridRow.appendChild(gridElement);
         }
     }
+}
+
+for (const button of ui.sizeButtons) {
+    button.addEventListener('click', () => {
+        resolveSizeButtonClick(button);
+    })
+}
+
+for (const button of ui.colorButtons) {
+    button.addEventListener('click', () => {
+        resolveColorButtonClick(button);
+    })
+}
+
+for (const button of ui.gridBackgroundButtons) {
+    button.addEventListener('click', () => {
+        resolveGridBackgroundButtonClick(button);
+    })
+}
+
+function resolveSizeButtonClick(button) {
+    for (const btn of ui.sizeButtons) {
+        btn.classList.remove('active');
+    }
+    button.classList.add('active');
+    console.log(button.id);
+}
+
+function resolveColorButtonClick(button) {
+    for (const btn of ui.colorButtons) {
+        btn.classList.remove('active');
+    }
+    button.classList.add('active');
+    console.log(button.id);
+}
+
+function resolveGridBackgroundButtonClick(button) {
+    for (const btn of ui.gridBackgroundButtons) {
+        btn.classList.remove('active');
+    }
+    button.classList.add('active');
+    console.log(button.id);
 }
 
 function deleteGrid() {

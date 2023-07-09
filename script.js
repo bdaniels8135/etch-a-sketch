8c875674264d3722cf8b2caefdca08f09cd1f967
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-07-08 20:25:20 -0400

    Rework basic JS from changes to index.html

--- script.js
+++ script.js
@@ -2,10 +2,35 @@ const ui = Object.freeze({
     gridContainer: document.querySelector('#grid-container'),
     sizeButtons: document.querySelectorAll('.button.size'),
     colorButtons: document.querySelectorAll('.button.color'),
-    gridBackgroundButtons: document.querySelectorAll('.button.grid-background')
+    backgroundColorButtons: document.querySelectorAll('.button.grid-background'),
+    eraseButton: document.querySelector('#erase-button')
 })
 
-function buildGrid(dimension = 16) {
+function addButtonClickListeners() {
+    for (const button of ui.sizeButtons) {
+        button.addEventListener('click', () => {
+            resolveSizeButtonClick(button);
+        })
+    }
+    
+    for (const button of ui.colorButtons) {
+        button.addEventListener('click', () => {
+            resolveColorButtonClick(button);
+        })
+    }
+    
+    for (const button of ui.backgroundColorButtons) {
+        button.addEventListener('click', () => {
+            resolveBackgroundColorButtonClick(button);
+        })
+    }
+    
+    ui.eraseButton.addEventListener('click', () => {
+        resolveEraseButtonClick()
+    })    
+}
+
+function buildGrid(dimension = 25) {
     let gridRow;
     let gridElement;
     for (let i = 0; i < dimension; i++) {
@@ -18,48 +43,6 @@ function buildGrid(dimension = 16) {
             gridRow.appendChild(gridElement);
         }
     }
-}
-
-for (const button of ui.sizeButtons) {
-    button.addEventListener('click', () => {
-        resolveSizeButtonClick(button);
-    })
-}
-
-for (const button of ui.colorButtons) {
-    button.addEventListener('click', () => {
-        resolveColorButtonClick(button);
-    })
-}
-
-for (const button of ui.gridBackgroundButtons) {
-    button.addEventListener('click', () => {
-        resolveGridBackgroundButtonClick(button);
-    })
-}
-
-function resolveSizeButtonClick(button) {
-    for (const btn of ui.sizeButtons) {
-        btn.classList.remove('active');
-    }
-    button.classList.add('active');
-    console.log(button.id);
-}
-
-function resolveColorButtonClick(button) {
-    for (const btn of ui.colorButtons) {
-        btn.classList.remove('active');
-    }
-    button.classList.add('active');
-    console.log(button.id);
-}
-
-function resolveGridBackgroundButtonClick(button) {
-    for (const btn of ui.gridBackgroundButtons) {
-        btn.classList.remove('active');
-    }
-    button.classList.add('active');
-    console.log(button.id);
 }
 
 function deleteGrid() {
@@ -88,6 +71,35 @@ function fillGrid() {
     }
 }
 
+function resolveSizeButtonClick(button) {
+    for (const btn of ui.sizeButtons) {
+        btn.classList.remove('active');
+    }
+    button.classList.add('active');
+    console.log(button.id);
+}
+
+function resolveColorButtonClick(button) {
+    for (const btn of ui.colorButtons) {
+        btn.classList.remove('active');
+    }
+    button.classList.add('active');
+    console.log(button.id);
+}
+
+function resolveBackgroundColorButtonClick(button) {
+    for (const btn of ui.gridBackgroundButtons) {
+        btn.classList.remove('active');
+    }
+    button.classList.add('active');
+    console.log(button.id);
+}
+
+function resolveEraseButtonClick() {
+    console.log(ui.eraseButton.id);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     buildGrid();
+    addButtonClickListeners();
 })
